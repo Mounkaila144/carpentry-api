e352eb66d9ebba29848871be3fe1bef4023aad5e
Mounkaila <mounkaila144@gmail.com>
2025-12-26 19:13:08 +0100

    CMS

--- bootstrap/cache/modules.php
+++ bootstrap/cache/modules.php
@@ -1,11 +1,13 @@
 <?php return array (
   'providers' => 
   array (
-    0 => 'Modules\\UsersGuard\\Providers\\UsersGuardServiceProvider',
+    0 => 'Modules\\Cms\\Providers\\CmsServiceProvider',
+    1 => 'Modules\\UsersGuard\\Providers\\UsersGuardServiceProvider',
   ),
   'eager' => 
   array (
-    0 => 'Modules\\UsersGuard\\Providers\\UsersGuardServiceProvider',
+    0 => 'Modules\\Cms\\Providers\\CmsServiceProvider',
+    1 => 'Modules\\UsersGuard\\Providers\\UsersGuardServiceProvider',
   ),
   'deferred' => 
   array (
